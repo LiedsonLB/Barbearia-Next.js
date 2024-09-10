004a2746640108854af4a7ecffd7d41e7700077c
LiedsonLB <liedson.b9@gmail.com>
2024-09-09 22:44:16 -0300

    feat: add inital booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
+import BookingItem from "../_components/Booking-item";
 
 export default function Home() {
   return (
@@ -18,6 +19,10 @@ export default function Home() {
       </div>
       <div className="px-5 mt-6">
         <Search />
+      </div>
+      <div className="p-5">
+        <h2 className="text-sm uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
+        <BookingItem />
       </div>
 
     </div>
---
+++ app/_components/Booking-item.tsx
@@ -0,0 +1,35 @@
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "./ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const BookingItem = () => {
+    return (
+        <Card>
+            <CardContent className="p-5 flex justify-between py-0">
+
+                <div className="flex flex-col gap-2 py-5">
+                    <Badge className="bg-[#221c3d] text-primary hover:bg-[#221c3d] w-fit">Confirmado</Badge>
+                    <h2 className="font-bold">Corte de cabelo</h2>
+
+                    <div className="flex items-center gap-2">
+                        <Avatar className="h-6 w-6">
+                            <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+
+                            <AvatarFallback>A</AvatarFallback>
+                        </Avatar>
+
+                        <h3 className="text-sm">Vintage Barber</h3>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-center px-3 border-l border-solid border-secondary">
+                    <p className="text-sm">Fevereiro</p>
+                    <p className="text-2xl">06</p>
+                    <p className="text-sm">09:45</p>
+                </div>
+
+            </CardContent>
+        </Card>
+    );
+}
+ 
+export default BookingItem;
